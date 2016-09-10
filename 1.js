794ce8347a31499ebaf144f3934091286f3d6333
Andrew Tobilko <andrew.tobilko@gmail.com>
2016-09-10 19:01:22 +0300

    1 lab

---
+++ 1.js
@@ -0,0 +1,36 @@
+var app = angular.module("app", []);
+
+app.controller('FirstLabController', function() {
+
+    var self = this, DEFAULT_VALUE = 0;
+
+    self.dimension = 9;
+
+    function isAllowedToPutNumber(i, j, d) {
+        function isRightPart() { return j != i && i > j && j + i < d - 1; }
+        function isLeftPart() { return i < j && j + i > d - 1; }
+
+        return isRightPart() || isLeftPart();
+    }
+
+    self.refill = function () {
+        var counter = 0;
+        for (var i = 0; i <  self.dimension; ++i) {
+            if (!self.matrix[i]) self.matrix[i] = [];
+            for (var j = 0; j <  self.dimension; ++j) {
+                self.matrix[i][j] = isAllowedToPutNumber(i, j, self.dimension) ? ++counter : DEFAULT_VALUE;
+            }
+        }
+    };
+    self.fill = function () {
+        self.matrix = [];
+        for (var i = 0; i < self.dimension; ++i) {
+            self.matrix[i] = new Array(self.dimension).fill(DEFAULT_VALUE);
+        }
+    };
+
+    self.isEmpty = function(i, j) {
+        return self.matrix[i][j] == 0;
+    }
+
+});
