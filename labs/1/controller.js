4735d2f2118bf981c255e8c6bd4ee17b34bd8aae
Andrew Tobilko <andrew.tobilko@gmail.com>
2016-09-15 00:33:01 +0300

    quick tracing

--- labs/1/controller.js
+++ labs/1/controller.js
@@ -10,6 +10,14 @@ angular.module('15501550').controller('FirstLabController', function() {
 
         return isRightPart() || isLeftPart();
     }
+    function printMatrixToConsole(matrix) {
+        var out = "";
+        for(var i = 0; i < matrix.length; ++i) {
+            for (var j = 0; j < matrix[i].length; ++j) out += matrix[i][j] + " ";
+            out += "\n";
+        }
+        console.log(out);
+    }
 
     self.refill = function () {
         var counter = 0;
@@ -19,13 +27,16 @@ angular.module('15501550').controller('FirstLabController', function() {
                 self.matrix[i][j] = isAllowedToPutNumber(i, j, self.dimension) ? ++counter : DEFAULT_VALUE;
             }
         }
+        printMatrixToConsole(self.matrix);
     };
     self.fill = function () {
         self.matrix = [];
         for (var i = 0; i < self.dimension; ++i) {
             self.matrix[i] = new Array(self.dimension).fill(DEFAULT_VALUE);
         }
+        printMatrixToConsole(self.matrix);
     };
+
     self.isEmpty = function(i, j) {
         return self.matrix[i][j] == 0;
     }
