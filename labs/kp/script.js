2bb436fdc11fa53735130e4a855dea85dd0dd1ef
Andrew Tobilko <andrew.tobilko@gmail.com>
2016-12-28 14:16:32 +0200

    the speedy exam

--- labs/kp/script.js
+++ labs/kp/script.js
@@ -1,3 +1,30 @@
-/**
- * Created by Andrew Tobilko on 12/27/2016.
- */
+window.addEventListener('load', function () {
+    document.getElementById('text1').onclick = function () {
+        document.getElementById('input1').value = '';
+        document.getElementById('text1').classList.add('hidden');
+    }
+    var i2 = document.getElementById('input2');
+    i2.onfocus = function () {
+        var placeholder = document.getElementById('placeholder');
+        placeholder.classList.add('placeholder');
+    };
+    i2.onblur = function () {
+        if (document.getElementById('input2').value.length === 0) {
+            var placeholder = document.getElementById('placeholder');
+            placeholder.classList.remove('placeholder');
+        }
+    }
+}, false);
+
+
+function handleFirstTask() {
+    var input = document.getElementById('input1');
+    var inputValue = input.value;
+    var styles = document.getElementById('text1').classList;
+
+    if (inputValue.length == 0) {
+        styles.add('hidden');
+    } else {
+        styles.remove('hidden');
+    }
+}
